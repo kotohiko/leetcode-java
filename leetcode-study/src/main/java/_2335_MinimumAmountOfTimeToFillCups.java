e003648efac7521ab497678c06ac15458b26e61f
jacobsuen <tachibana.kotohiko@gmail.com>
2023-02-11 02:33:54 +0800

    0211

---
+++ leetcode-study/src/main/java/_2335_MinimumAmountOfTimeToFillCups.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+/**
+ * <h2>2335. 装满杯子需要的最短总时长</h2>
+ * <p>
+ * <a href="https://leetcode.cn/problems/minimum-amount-of-time-to-fill-cups/">LeetCode link</a>
+ */
+class MinimumAmountOfTimeToFillCups_Solution {
+    public int fillCups(int[] amount) {
+        Arrays.sort(amount);
+        if (amount[0] + amount[1] <= amount[2]) {
+            return amount[2];
+        }
+        return ((amount[0] + amount[1] + amount[2] + 1) / 2);
+    }
+}
--- leetcode-study/src/test/java/_2335_MinimumAmountOfTimeToFillCupsTest.java
+++ leetcode-study/src/test/java/_2335_MinimumAmountOfTimeToFillCupsTest.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 /**
  * @author Leetcode
  */
-public class _0001_TwoSumTest {
+public class _2335_MinimumAmountOfTimeToFillCupsTest {
     public static int[] stringToIntegerArray(String input) {
         input = input.trim();
         input = input.substring(1, input.length() - 1);
@@ -22,34 +22,15 @@ public class _0001_TwoSumTest {
         return output;
     }
 
-    public static String integerArrayToString(int[] nums, int length) {
-        if (length == 0) {
-            return "[]";
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (int index = 0; index < length; index++) {
-            int number = nums[index];
-            result.append(number).append(", ");
-        }
-        return "[" + result.substring(0, result.length() - 2) + "]";
-    }
-
-    public static String integerArrayToString(int[] nums) {
-        return integerArrayToString(nums, nums.length);
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = in.readLine()) != null) {
-            int[] nums = stringToIntegerArray(line);
-            line = in.readLine();
-            int target = Integer.parseInt(line);
+            int[] amount = stringToIntegerArray(line);
 
-            int[] ret = new TwoSum_Solution().twoSum(nums, target);
+            int ret = new MinimumAmountOfTimeToFillCups_Solution().fillCups(amount);
 
-            String out = integerArrayToString(ret);
+            String out = String.valueOf(ret);
 
             System.out.print(out);
         }
