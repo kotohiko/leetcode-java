35153d21fd9670363cef8ddd332cc7fb422822ed
kotohiko <tachibana.kotohiko@gmail.com>
2023-09-20 11:33:03 +0800

    重构leetcode-java

---
+++ lc1-100/src/main/java/_0001_TwoSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+/**
+ * <h2>1. Two Sum</h2>
+ * <a href="https://leetcode.cn/problems/two-sum/">LeetCode link</a>
+ *
+ * @author Jacob Suen
+ * @create 10:55 20/09/2023
+ */
+class TwoSum_Solution {
+    public int[] twoSum(int[] nums, int target) {
+        var map = new HashMap<Integer, Integer>();
+        var n = nums.length;
+        for (var i = 0; i < n; ++i) {
+            var b = target - nums[i];
+            if (map.containsKey(b)) {
+                return new int[]{map.get(b), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
