968c57ec58f6dea8588525fbab2426942e575e8a
kotohiko <tachibana.kotohiko@gmail.com>
2023-09-25 08:10:34 +0800

    58

---
+++ lc1-100/src/main/java/_0058_LengthOfLastWord.java
@@ -0,0 +1,21 @@
+/**
+ * <h2>58. Length of Last Word</h2>
+ * <a href="https://leetcode.cn/problems/length-of-last-word/">LeetCode link</a>
+ *
+ * @author Jacob Suen
+ * @since 18:37 23/09/2023
+ */
+class LengthOfLastWord_Solution {
+    public int lengthOfLastWord(String s) {
+        var n = s.length();
+        var i = n - 1;
+        while (i >= 0 && Character.isSpaceChar(s.charAt(i))) {
+            --i;
+        }
+        var j = i;
+        while (j >= 0 && Character.isLetter(s.charAt(j))) {
+            --j;
+        }
+        return i - j;
+    }
+}
