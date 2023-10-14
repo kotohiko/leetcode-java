2190dc06312889848b366a9bc803c84593a6d08d
kotohiko <tachibana.kotohiko@gmail.com>
2023-10-14 23:42:42 +0800

    1014 daily challenge

---
+++ lc101-200/src/main/java/_0136_SingleNumber.java
@@ -0,0 +1,16 @@
+/**
+ * <h2>136. Single Number</h2>
+ * <a href="https://leetcode.cn/problems/single-number">LeetCode link</a>
+ *
+ * @author Jacob Suen
+ * @since 23:38 Oct 14, 2023
+ */
+class SingleNumber_Solution {
+    public int singleNumber(int[] nums) {
+        var single = 0;
+        for (var num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
