8f94b6216c17de9a554c443e8edd2165d5abe75a
kotohiko <tachibana.kotohiko@gmail.com>
2023-12-01 17:20:47 +0800

    Dec 1st 2023 daily challenge

---
+++ lc2601-2700/src/main/java/_2661_FirstCompletelyPaintedRowOrColumn.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+/**
+ * <h2>2661. First Completely Painted Row or Column</h2>
+ * <a href="https://leetcode.cn/problems/first-completely-painted-row-or-column/">LeetCode link</a>
+ *
+ * @author Jacob Suen
+ * @since 16:46 Dec 01, 2023
+ */
+class FirstCompletelyPaintedRowOrColumn_Solution {
+    public int firstCompleteIndex(int[] arr, int[][] mat) {
+        //
+        var n = mat.length;
+        var m = mat[0].length;
+        var map = new HashMap<Integer, int[]>();
+        for (var i = 0; i < n; ++i) {
+            for (var j = 0; j < m; ++j) {
+                map.put(mat[i][j], new int[]{i, j});
+            }
+        }
+        var c1 = new int[n];
+        var c2 = new int[m];
+        for (var i = 0; i < n * m; ++i) {
+            var info = map.get(arr[i]);
+            var x = info[0];
+            var y = info[1];
+            if (++c1[x] == m || ++c2[y] == n) {
+                return i;
+            }
+        }
+        return -1; // never
+    }
+}
---
+++ lc2601-2700/src/test/java/_2661_FirstCompletelyPaintedRowOrColumnTest.java
@@ -0,0 +1,56 @@
+import com.eclipsesource.json.JsonArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author LeetCode Playground
+ */
+public class _2661_FirstCompletelyPaintedRowOrColumnTest {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.isEmpty()) {
+            return new int[0];
+        }
+
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for (int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static int[][] stringToInt2dArray(String input) {
+        JsonArray jsonArray = JsonArray.readFrom(input);
+        if (jsonArray.isEmpty()) {
+            return new int[0][0];
+        }
+
+        int[][] arr = new int[jsonArray.size()][];
+        for (int i = 0; i < arr.length; i++) {
+            JsonArray cols = jsonArray.get(i).asArray();
+            arr[i] = stringToIntegerArray(cols.toString());
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int[] arr = stringToIntegerArray(line);
+            line = in.readLine();
+            int[][] mat = stringToInt2dArray(line);
+
+            int ret = new FirstCompletelyPaintedRowOrColumn_Solution().firstCompleteIndex(arr, mat);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
