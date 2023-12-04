24cfb6476dce8e3c3cff857fbfac56f63e98fc44
kotohiko <tachibana.kotohiko@gmail.com>
2023-12-04 15:48:44 +0800

    Dec 4th 2023 daily challenge

---
+++ lc1001-1100/src/main/java/_1038_BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,25 @@
+/**
+ * <h2>1038. Binary Search Tree to Greater Sum Tree</h2>
+ * <a href="https://leetcode.cn/problems/binary-search-tree-to-greater-sum-tree/">LeetCode link</a>
+ *
+ * @author Jacob Suen
+ * @since 09:03 12月 04, 2023
+ */
+class BinarySearchTreeToGreaterSumTree_Solution {
+    int tot = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.right);
+        tot += root.val;
+        root.val = tot;
+        dfs(root.left);
+    }
+}
