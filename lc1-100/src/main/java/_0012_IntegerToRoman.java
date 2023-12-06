086f3c142f2bdccb3ba1f6ffbd68eee135bda8bc
kotohiko <tachibana.kotohiko@gmail.com>
2023-12-06 18:03:43 +0800

    12

--- lc1-100/src/main/java/_0012_IntegerToRoman.java
+++ lc1-100/src/main/java/_0012_IntegerToRoman.java
@@ -11,7 +11,7 @@ class IntegerToRoman_Solution {
         int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         StringBuilder stringBuilder = new StringBuilder();
-        int index = 0;
+        var index = 0;
         while (index < 13) {
             // 特别注意：这里是等号
             while (num >= nums[index]) {
