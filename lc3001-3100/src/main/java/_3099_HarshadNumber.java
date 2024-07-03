bb1efd67c3bc9b11bcd16b08603c8e7fc892be39
JacobSuen <tachibana.kotohiko@gmail.com>
2024-07-03 08:06:22 +0800

    7.3 daily challenge

---
+++ lc3001-3100/src/main/java/_3099_HarshadNumber.java
@@ -0,0 +1,17 @@
+/**
+ * <a href="https://leetcode.cn/problems/harshad-number"><h1>3099. 哈沙德数</h1></a>
+ *
+ * @author Jacob Suen
+ * @since 07:45 Jul 03, 2024
+ */
+class HarshadNumber_Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        var sum = 0;
+        var y = x;
+        while (x > 0) {
+            sum += x % 10;
+            x /= 10;
+        }
+        return y % sum == 0 ? sum : -1;
+    }
+}
---
+++ lc3001-3100/src/test/java/_3099_HarshadNumberTest.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author LeetCode Playground
+ */
+public class _3099_HarshadNumberTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int x = Integer.parseInt(line);
+
+            int ret = new HarshadNumber_Solution().sumOfTheDigitsOfHarshadNumber(x);
+
+            String out = String.valueOf(ret);
+
+            System.out.println(out);
+        }
+    }
+}
