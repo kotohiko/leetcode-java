a5976cc6f38d65d0d59fd8be97ba6a6db9f1248f
JacobSuen <tachibana.kotohiko@gmail.com>
2024-07-21 10:36:58 +0800

    7.21 daily challenge

---
+++ lc1101-1200/src/main/java/_1186_MaximumSubarraySumWithOneDeletion.java
@@ -0,0 +1,20 @@
+/**
+ * <a href="https://leetcode.cn/problems/maximum-subarray-sum-with-one-deletion">
+ * <h1>1186. Maximum Subarray Sum with One Deletion</h1></a>
+ *
+ * @author Jacob Suen
+ * @since 10:19 Jul 21, 2024
+ */
+class MaximumSubarraySumWithOneDeletion_Solution {
+    public int maximumSum(int[] arr) {
+        var dp0 = arr[0];
+        var dp1 = 0;
+        var res = arr[0];
+        for (var i = 1; i < arr.length; ++i) {
+            dp1 = Math.max(dp0, dp1 + arr[i]);
+            dp0 = Math.max(dp0, 0) + arr[i];
+            res = Math.max(res, Math.max(dp0, dp1));
+        }
+        return res;
+    }
+}
---
+++ lc1101-1200/src/test/java/_1186_MaximumSubarraySumWithOneDeletionTest.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author LeetCode Playground
+ */
+public class _1186_MaximumSubarraySumWithOneDeletionTest {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.isEmpty()) {
+            return new int[0];
+        }
+
+        var parts = input.split(",");
+        var output = new int[parts.length];
+        for (var index = 0; index < parts.length; index++) {
+            var part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws IOException {
+        var in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            var arr = stringToIntegerArray(line);
+
+            var ret = new MaximumSubarraySumWithOneDeletion_Solution().maximumSum(arr);
+
+            var out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
