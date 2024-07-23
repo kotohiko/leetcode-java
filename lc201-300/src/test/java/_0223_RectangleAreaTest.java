fe85d6f655da890eeeed9b6421eeb68f9ca6f68a
kotohiko <tachibana.kotohiko@gmail.com>
2024-07-23 11:07:40 +0800

    Problem 223 added

--- lc201-300/src/test/java/_0223_RectangleAreaTest.java
+++ lc201-300/src/test/java/_0223_RectangleAreaTest.java
@@ -7,28 +7,28 @@ import java.io.InputStreamReader;
  */
 public class _0223_RectangleAreaTest {
     public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        var in = new BufferedReader(new InputStreamReader(System.in));
         String line;
         while ((line = in.readLine()) != null) {
-            int ax1 = Integer.parseInt(line);
+            var ax1 = Integer.parseInt(line);
             line = in.readLine();
-            int ay1 = Integer.parseInt(line);
+            var ay1 = Integer.parseInt(line);
             line = in.readLine();
-            int ax2 = Integer.parseInt(line);
+            var ax2 = Integer.parseInt(line);
             line = in.readLine();
-            int ay2 = Integer.parseInt(line);
+            var ay2 = Integer.parseInt(line);
             line = in.readLine();
-            int bx1 = Integer.parseInt(line);
+            var bx1 = Integer.parseInt(line);
             line = in.readLine();
-            int by1 = Integer.parseInt(line);
+            var by1 = Integer.parseInt(line);
             line = in.readLine();
-            int bx2 = Integer.parseInt(line);
+            var bx2 = Integer.parseInt(line);
             line = in.readLine();
-            int by2 = Integer.parseInt(line);
+            var by2 = Integer.parseInt(line);
 
-            int ret = new RectangleArea_Solution().computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
+            var ret = new RectangleArea_Solution().computeArea(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2);
 
-            String out = String.valueOf(ret);
+            var out = String.valueOf(ret);
 
             System.out.println(out);
         }
